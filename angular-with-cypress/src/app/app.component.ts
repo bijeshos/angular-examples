d4870df984fcc41294667bcc1cb7f38e977c0f2f
Bijesh O S <bijeshos@users.noreply.github.com>
2020-02-11 19:30:16 +0530

    cleaned up UI structure and data

--- angular-with-cypress/src/app/app.component.ts
+++ angular-with-cypress/src/app/app.component.ts
@@ -6,37 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-n-cypress-v02';
+  title = 'Angular with Cypress';
 
 columnDefs = [
-  {headerName: 'Meta 1', field: 'meta_1', editable: true },
-  {headerName: 'Meta 2', field: 'meta_2', editable: true },
-  {headerName: 'Meta 3', field: 'meta_3', editable: true},
-  {headerName: 'Value 1', field: 'value_1', editable: true},
-  {headerName: 'Value 2', field: 'value_2', editable: true},
-  {headerName: 'Value 3', field: 'value_3', editable: true},
-  {headerName: 'Value 4', field: 'value_4', editable: true},
-  {headerName: 'Value 5', field: 'value_5', editable: true},
-  {headerName: 'Value 6', field: 'value_6', editable: true},
-  
+  {headerName: 'Item Name', field: 'item_name', editable: true },
+  {headerName: 'Jan', field: 'volume_jan', editable: true},
+  {headerName: 'Feb', field: 'volume_feb', editable: true},
+  {headerName: 'Mar', field: 'volume_mar', editable: true},
+  {headerName: 'Apr', field: 'volume_apr', editable: true},
+  {headerName: 'May', field: 'volume_may', editable: true},
+  {headerName: 'Jun', field: 'volume_jun', editable: true},
+  {headerName: 'Jul', field: 'volume_jul', editable: true},
   {headerName: 'Total', field: 'total', editable: true},
 ];
 
 rowData = [
-  { meta_1:'', meta_2: 'Section-1', meta_3: '', value_1: '', value_2: '', value_3: '', value_4: '', value_5: '', value_6: '', total: '' },
-  { meta_1:'', meta_2: 'Item-A1', meta_3: 'xyz', value_1: '', value_2: '', value_3: '', value_4: '', value_5: '', value_6: '', total: '10' },
-  { meta_1:'', meta_2: 'Item-B1', meta_3: 'xyz', value_1: '', value_2: '', value_3: '', value_4: '', value_5: '', value_6: '', total: '5' },
-  { meta_1:'', meta_2: 'Item-C1', meta_3: 'xyz', value_1: '', value_2: '', value_3: '', value_4: '', value_5: '', value_6: '', total: '5' },
-  { meta_1:'', meta_2: 'Item-D1', meta_3: 'xyz', value_1: '', value_2: '', value_3: '', value_4: '', value_5: '', value_6: '', total: '5' },
-  { meta_1:'', meta_2: 'Item-E1', meta_3: 'xyz', value_1: '', value_2: '', value_3: '', value_4: '', value_5: '', value_6: '', total: '5' },
-  { meta_1:'', meta_2: 'Item-F1', meta_3: 'xyz', value_1: '', value_2: '', value_3: '', value_4: '', value_5: '', value_6: '', total: '5' },
-  { meta_1:'', meta_2: 'Item-G1', meta_3: 'xyz', value_1: '', value_2: '', value_3: '', value_4: '', value_5: '', value_6: '', total: '5' },
-  { meta_1:'', meta_2: 'Section-2', meta_3: '', value_1: '', value_2: '', value_3: '', value_4: '', value_5: '', value_6: '', total: '' },
-  { meta_1:'', meta_2: 'Item-H2', meta_3: 'xyz', value_1: '', value_2: '', value_3: '', value_4: '', value_5: '', value_6: '', total: '5' },
-  { meta_1:'', meta_2: 'Item-I2', meta_3: 'xyz', value_1: '', value_2: '', value_3: '', value_4: '', value_5: '', value_6: '', total: '5' },
-  { meta_1:'', meta_2: 'Section-1 Total', meta_3: 'xyz', value_1: '14', value_2: '7', value_3: '7', value_4: '7', value_5: '7', value_6: '7', total: '35' },
-  { meta_1:'', meta_2: 'Section-2 Total', meta_3: 'xyz', value_1: '4', value_2: '2', value_3: '2', value_4: '2', value_5: '2', value_6: '2', total: '4' },
-  { meta_1:'', meta_2: 'Balance', meta_3: 'xyz', value_1: '5', value_2: '5', value_3: '5', value_4: '5', value_5: '5', value_6: '5', total: '31' },
+  { item_name: 'Coca Cola', volume_jan: '', volume_feb: '', volume_mar: '', volume_apr: '', volume_may: '', volume_jun: '', volume_jul: '', total: '' },
+  { item_name: 'Pepsi', volume_jan: '', volume_feb: '', volume_mar: '', volume_apr: '', volume_may: '', volume_jun: '', volume_jul: '', total: '' },
+  { item_name: 'Sprite', volume_jan: '', volume_feb: '', volume_mar: '', volume_apr: '', volume_may: '', volume_jun: '', volume_jul: '', total: '' },
+  { item_name: 'Fanta', volume_jan: '', volume_feb: '', volume_mar: '', volume_apr: '', volume_may: '', volume_jun: '', volume_jul: '', total: '' },
+  { item_name: 'Bisleri', volume_jan: '', volume_feb: '', volume_mar: '', volume_apr: '', volume_may: '', volume_jun: '', volume_jul: '', total: '' },
+  { item_name: 'Aquafina', volume_jan: '', volume_feb: '', volume_mar: '', volume_apr: '', volume_may: '', volume_jun: '', volume_jul: '', total: '' },
 ];
 
 }
