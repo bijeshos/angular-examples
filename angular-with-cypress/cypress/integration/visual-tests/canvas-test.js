4ee769156e72004bb1607ec09247f977f8e9e994
Bijesh O S <bijeshos@users.noreply.github.com>
2020-02-12 22:46:17 +0530

    added canvas testing code

---
+++ angular-with-cypress/cypress/integration/visual-tests/canvas-test.js
@@ -0,0 +1,23 @@
+/// <reference types="Cypress" />
+
+context('Canvas - Visual Test', () => {
+
+  it('Visual Test : Scenario 01', () => {
+
+    //cy.viewport(1200,1500)
+    cy.visit('/')
+      .then(()=>{
+        cy.get('[data-cy=canvas-01]')
+        .toMatchImageSnapshot(
+          {
+            "createDiffImage": true,
+            "threshold": 0.01,
+            "name":"canvas-01-v01",
+            "thresholdType": "percent"
+
+          }
+        )
+      })
+  })
+
+})
